1272759cdbf9ca35ba02961214b4d2fec3049df5
Akila-Wasalathilaka <akilawasalathilaka@gmail.com>
2025-08-18 00:28:00 +0530

    fx

--- SlumSeg/kaggle_analysis.py
+++ SlumSeg/kaggle_analysis.py
@@ -4,6 +4,4 @@
 # Clone repo and run analysis
 !git clone https://github.com/Akila-Wasalathilaka/Slum-detection-model-using-UNET.git
 %cd Slum-detection-model-using-UNET/SlumSeg
-
-# Dataset path already set to /kaggle/input/data
-!python kaggle_analysis_pipeline.py
+!python kaggle_analysis_simple.py
--- SlumSeg/kaggle_training.py
+++ SlumSeg/kaggle_training.py
@@ -4,6 +4,4 @@
 # Clone repo and run training
 !git clone https://github.com/Akila-Wasalathilaka/Slum-detection-model-using-UNET.git
 %cd Slum-detection-model-using-UNET/SlumSeg
-
-# Dataset path already set to /kaggle/input/data
-!python kaggle_train_only.py
+!python kaggle_train_simple.py
